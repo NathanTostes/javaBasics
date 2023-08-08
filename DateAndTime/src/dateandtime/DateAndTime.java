aa0aaa78357b68b568c5f8ed5bd20985a1946bb0
NathanPereiraT <113456933+NathanPereiraT@users.noreply.github.com>
2023-08-08 16:43:56 -0300

    get system date and time in java

---
+++ DateAndTime/src/dateandtime/DateAndTime.java
@@ -0,0 +1,17 @@
+
+package dateandtime;
+
+import java.util.Date;
+
+public class DateAndTime {
+
+    public static void main(String[] args) {
+
+        Date clock = new Date();
+        
+        System.out.print("Hora do sistema: ");
+        System.out.println(clock.toString());
+        
+    }
+    
+}
