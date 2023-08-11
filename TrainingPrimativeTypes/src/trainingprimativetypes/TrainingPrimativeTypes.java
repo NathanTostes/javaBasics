53212e93a12abac8060635a3a34a9c7765b40bed
NathanPereiraT <113456933+NathanPereiraT@users.noreply.github.com>
2023-08-11 13:40:06 -0300

    training primative types

---
+++ TrainingPrimativeTypes/src/trainingprimativetypes/TrainingPrimativeTypes.java
@@ -0,0 +1,21 @@
+package trainingprimativetypes;
+
+/**
+ * @author Nathan
+ */
+
+public class TrainingPrimativeTypes {
+
+    public static void main(String[] args) {
+        
+        String word = "11";
+        int number = Integer.parseInt(word);
+        System.out.println(number);
+        
+        Integer numero = 11;
+        String palavra = numero.toString();
+        System.out.println(palavra);
+        
+    }
+    
+}
