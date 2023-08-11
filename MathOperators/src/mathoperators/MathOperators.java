a5793c1886cd0ac3157427ca127023327eaedf85
NathanPereiraT <113456933+NathanPereiraT@users.noreply.github.com>
2023-08-11 15:47:57 -0300

    math operators

---
+++ MathOperators/src/mathoperators/MathOperators.java
@@ -0,0 +1,27 @@
+package mathoperators;
+
+/**
+ * @author Nathan
+ */
+
+public class MathOperators {
+
+    public static void main(String[] args) {
+        
+        Float n1 = 5f;
+        Float n2 = 10f;
+        Float media = (n1 + n2) / 2 ;
+        System.out.println(media);
+        
+        int num = 10;
+        num++;
+        System.out.println(num);
+        
+        int x = 4;
+        int y = 6;
+        x += y;
+        System.out.println(x);
+        
+    }
+    
+}
