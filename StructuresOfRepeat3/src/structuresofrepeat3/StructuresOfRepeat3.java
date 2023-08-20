478893cf549e9b6c4ac0f536caf8c68d696bda44
NathanPereiraT <113456933+NathanPereiraT@users.noreply.github.com>
2023-08-19 23:05:00 -0300

    structures of repeat 3

---
+++ StructuresOfRepeat3/src/structuresofrepeat3/StructuresOfRepeat3.java
@@ -0,0 +1,20 @@
+package structuresofrepeat3;
+
+/**
+ *
+ * @author Nathan
+ * 
+ */
+
+public class StructuresOfRepeat3 {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        for (int num = 1; num <= 5; ++num){
+            for (int val = 1; val <= 5; ++val){
+                System.out.println(num + " - " + val);
+            }
+        }
+    }
+    
+}
