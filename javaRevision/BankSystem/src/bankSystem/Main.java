4845a7232d138a15a1e9e4e6e8404540cf857f4a
NathanTostes <nathanpt777@gmail.com>
2024-05-20 21:38:01 -0300

    test

--- javaRevision/BankSystem/src/bankSystem/Main.java
+++ javaRevision/BankSystem/src/bankSystem/Main.java
@@ -4,7 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		EmployeeAuthenticable a1 = new Manager("Nathan", "32489408349", 10000, "8008");
 		System.out.println(a1.toString());
-		Account clientAccount = new CurrentAccount("Marcos", "32923042893", 3204);
+		Account clientAccount = new CurrentAccount("Nathan", "32923042893", 3204);
 		try {
 			clientAccount.deposit(500);
 		} catch (InvalidValueException exception) {
