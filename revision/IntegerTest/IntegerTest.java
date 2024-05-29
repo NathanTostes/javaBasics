6d1ce62ce9ffa41a68ab565e5c4149b373f4dfb3
NathanTostes <nathanpt777@gmail.com>
2024-05-29 19:14:05 -0300

    (study) using Integer class

---
+++ revision/IntegerTest/IntegerTest.java
@@ -0,0 +1,9 @@
+public class IntegerTest {
+	public static void main(String[] args) {
+		Integer num1 = new Integer(1);
+		Integer num2 = new Integer(1);
+		
+		System.out.println(num1 == num2);
+		System.out.println(num1.equals(num2));
+	}
+}
