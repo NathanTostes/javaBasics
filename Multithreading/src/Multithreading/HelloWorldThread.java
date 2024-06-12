85efa764a3e1b1fef23d13a466d0d60e1e3748bb
NathanTostes <nathanpt777@gmail.com>
2024-06-12 12:20:13 -0300

    (Study) Multithreading

---
+++ Multithreading/src/Multithreading/HelloWorldThread.java
@@ -0,0 +1,24 @@
+package Multithreading;
+
+import java.time.Instant;
+import java.util.*;
+
+public class HelloWorldThread implements Runnable {
+	@Override
+	public void run() {
+		String threadName = Thread.currentThread().getName();
+		Collection<Integer> collection = new ArrayList<>();
+		System.out.println("Starting execution...");
+		long startTime = Instant.now().toEpochMilli();
+		for (int i = 0; i < 60000; i++) {
+			collection.add(i);
+		}
+		for (Integer integer : collection) {
+			collection.contains(integer);
+		}
+		long finishTime = Instant.now().toEpochMilli();
+		String totalTime = (finishTime - startTime) / 1000 + "."
+				+ ((finishTime - startTime) - (((finishTime - startTime) / 1000) * 1000));
+		System.out.println("Thread " + threadName + " has finished in " + totalTime);
+	}
+}
